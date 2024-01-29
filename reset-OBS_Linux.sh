dbb6158554d2ff57617ec3a6c5295786fd312a66
Femboy Griefing <136980449+FemBoyGriefing@users.noreply.github.com>
2024-01-29 10:43:20 +0100

    Update reset-OBS_Linux.sh

--- reset-OBS_Linux.sh
+++ reset-OBS_Linux.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 
-# OBS Studio schließen
 pkill obs
-
-# OBS Konfigurationsordner finden
 cd ~/.config/obs-studio || exit
-
-# Konfiguration sichern (optional)
 cp -r . ../OBS_Backup/
-
-# Konfigurationsdateien löschen
 rm -rf * && rm -rf ./basic/*
-
-# Cache zurücksetzen (optional)
 rm -rf ./cache/*
-
-# OBS Studio neu starten
 obs &
